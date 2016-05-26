a88f1b570ad8d1441be455d2e4582c573afb374c
alan egan <alanegan72@yahoo.co.uk>
2016-05-26 08:55:40 +0100

    add in spring boot infrastructure

---
+++ src/main/java/com/valtech/VtShoppingBasketApplication.java
@@ -0,0 +1,12 @@
+package com.valtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtShoppingBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtShoppingBasketApplication.class, args);
+	}
+}
